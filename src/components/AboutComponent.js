3248731351bca648501987f1283f682f12a02973
jcagle81 <57734109+jcagle81@users.noreply.github.com>
2021-04-21 17:20:06 -0500

    fordemo

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -51,7 +51,7 @@ function About(props) {
                     <Card className="bg-light mt-3">
                         <CardBody>
                             <blockquote className="blockquote">
-                                <p className="mb-0">I will not follow where the path may lead, but I will go where there is no path, and I will leave a trail.</p>
+                                <p className="mb-0">I will not follow where the path may lead, but I will go where there is no path, and I will leave a trail!</p>
                                 <footer className="blockquote-footer">Muriel Strode,{' '}
                                     <cite title="Source Title">"Wind-Wafted Wild Flowers" -
                                     The Open Court, 1903</cite>
