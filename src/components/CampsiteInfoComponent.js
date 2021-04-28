1c238400bf34298700b406ed3b9e79894a539866
jcagle81 <57734109+jcagle81@users.noreply.github.com>
2021-04-28 10:07:10 -0500

    redux actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -27,6 +27,11 @@ class CommentForm extends Component {
             });
         }
 
+        handleSubmit(values) {
+            this.toggleModal();
+            this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+        }
+
         
     
     render(){
@@ -103,7 +108,7 @@ function RenderCampsite({campsite}) {
     );
 }
 
-function RenderComments({comments}){
+function RenderComments({comments, addComment, campsiteId}) {
         if (comments) {
             return(
                 <div class="col-md-5 m-1">
@@ -120,7 +125,7 @@ function RenderComments({comments}){
                     })}
                     </ul> 
 
-                    <CommentForm />
+                    <CommentForm campsiteId={campsiteId} addComment={addComment} />
 
                 </div>
             );
@@ -144,7 +149,11 @@ function CampsiteInfo(props) {
             </div>
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments comments={props.comments} />
+                    <RenderComments comments={props.comments}
+                        comments={props.comments}
+                        addComment={props.addComment}
+                        campsiteId={props.campsite.id}
+                    />
                 </div>
             </div>
         );
